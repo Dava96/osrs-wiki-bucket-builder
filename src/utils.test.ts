b1a7fc121aaeeeaa49d00f5f2c14a5520ed4e9fb
David <Davidlomath@hotmail.co.uk>
2025-11-22 21:36:08 +0000

    format...

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -1,6 +1,6 @@
-import { add } from './utils.js'
+import { add } from './utils.js';
 import { expect, test } from 'vitest';
 
-test("add", () => {
-    expect(add(1, 2)).toBe(3);
-})
+test('add', () => {
+  expect(add(1, 2)).toBe(3);
+});
